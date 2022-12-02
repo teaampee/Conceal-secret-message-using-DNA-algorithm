f2e2302954ffe5a8cbdb7ee4152d47a9cd50b609
teaampee <ismail0w249@gmail.com>
2022-12-02 16:04:00 +0300

    bb

--- graduation project/app.py
+++ graduation project/app.py
@@ -20,42 +20,65 @@ def bits2dna(x):
     
     return dna
 
-# def image_encrypt(ar):
-    
-#     for i in range(0,ar.shape[0],1):
-#         for j in range(0,ar.shape[1],1):
+def image_encrypt(im):
+    temp = np.zeros((im.shape[0]*2,im.shape[1]*2,im.shape[2]),np.uint8)
+    for i in range(0,im.shape[0]):
+        for j in range(0,im.shape[1]):
+            temprgb = im[i,j]
+            r = temprgb[0]
+            r = eightbitbinary(r)
+            r = bits2dna(r)
+            g = temprgb[1]
+            g = eightbitbinary(g)
+            g = bits2dna(g)
+            b = temprgb[2]  
+            b = eightbitbinary(b)
+            b = bits2dna(b)
+            # print(r)
+            temp[i*2,j*2]= (ord(r[0]),ord(g[0]),ord(b[0]))
+            temp[i*2+1,j*2]= (ord(r[1]),ord(g[1]),ord(b[1]))
+            temp[i*2,j*2+1]= (ord(r[2]),ord(g[2]),ord(b[2]))
+            temp[i*2+1,j*2+1]= (ord(r[3]),ord(g[3]),ord(b[3]))
+
+            # print(temp[i*2,j*2])
+            # print(temp[i*2+1,j*2])
+            # print(temp[i*2,j*2+1])
+            # print(temp[i*2+1,j*2+1])
+    return temp
+            
+                        
             
 
 
-im1 = im.open('web/graduation project/coolit.jpeg')
+im1 = im.open('coolit.jpeg')
 px = np.asarray(im1) 
 
-print(px.shape[0] , px.shape[1] , px.shape[2])
+# print(px.shape[0] , px.shape[1] , px.shape[2])
 
-test = np.zeros((px.shape[0]*2,px.shape[1]*2,px.shape[2]),np.uint8)
+test = image_encrypt(px)
+im2 = im.fromarray(test, mode="RGB")
+im2.save("siuu.jpeg")
 
-print(test.shape[0] , test.shape[1] , test.shape[2])
-print(px[4,4])
-print(test[4,4])
-test[4,4] = px[4,4]
-print(test[4,4])
+# print(im2.getpixel((4,4)))
+# print(test.shape[0] , test.shape[1] , test.shape[2])
+# print(px[4,4])
+# # print(test[4,4])
+# test[4,4] = px[4,4]
+# # print(test[4,4])
         
 
-ax = test[4,4]
-g = eightbitbinary(ax[0])
-print(bits2dna(g))
+# ax = test[4,4]
+# g = eightbitbinary(ax[0])
+# g = bits2dna(g)
+# print(g)
 
 
 
 
-# Base 2(binary)
-bin_a = eightbitbinary(ax[1])
-print(bin_a)
-print(int(bin_a, 2)) 
-
-
-print("s")
-
+# # Base 2(binary)
+# bin_a = eightbitbinary(ax[1])
+# print(bin_a)
+# print(int(bin_a, 2)) 
 
 
 # --------------------------------------
