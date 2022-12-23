3026efcd5d9499da53f90b05959857f0637f825b
teaampee <ismail0w249@gmail.com>
2022-12-23 21:07:28 +0300

    comments

--- graduation project/app.py
+++ graduation project/app.py
@@ -1,11 +1,19 @@
+# importing pillow to load images and numpy for array handling 
 from PIL import Image as im
 import numpy as np
+
+#transform the output of the bin() function into an 8 bit string for example:
+# bin(3) = 0b11 makes that into 00000011 
+# to prepare it for handling the DNA encryption function()
 def eightbitbinary(x):
     temp = bin(x)
     s = temp[2:]
     for i in range(0,10-len(temp)):
         s = "0" + s
     return s
+# transforms the 8 bits binary number into DNA encryption string
+# where (00 = a,01 = t,10 = g,11 = c ),, so for example:
+# 00000011 => 00,00,00,11 => a,a,a,c => aaac
 def bits2dna(x):
     dna=""
     for i in range(0,8,2):
@@ -19,6 +27,7 @@ def bits2dna(x):
             dna += "c"
     
     return dna
+# transorm the DNA string back into an integer
 def dna2int(x):
     num = ""
     for i in range(4):
@@ -32,6 +41,13 @@ def dna2int(x):
             num = num + "11"
     
     return int(num,2)
+# encrypts the image using the DNA function()
+# creates a new array that is double the size of the original image
+# goes through the pixels gets their RGB values 
+# transform the values of the RGB into a 4 letter string using the dna encryption function()
+# for example 3 = aaac
+# for every letter in the string it we get its numeric value in ASCII using ord()
+# and we put these 4 numbers as seperate value for RGB in 4 neighboring pixels in the new array
 
 def image_encrypt(im):
     temp = np.zeros((im.shape[0]*2,im.shape[1]*2,im.shape[2]),np.uint8)
@@ -47,17 +63,17 @@ def image_encrypt(im):
             b = temprgb[2]  
             b = eightbitbinary(b)
             b = bits2dna(b)
-            # print(r)
             temp[i*2,j*2]= (ord(r[0]),ord(g[0]),ord(b[0]))
             temp[i*2+1,j*2]= (ord(r[1]),ord(g[1]),ord(b[1]))
             temp[i*2,j*2+1]= (ord(r[2]),ord(g[2]),ord(b[2]))
             temp[i*2+1,j*2+1]= (ord(r[3]),ord(g[3]),ord(b[3]))
 
-            # print(temp[i*2,j*2])
-            # print(temp[i*2+1,j*2])
-            # print(temp[i*2,j*2+1])
-            # print(temp[i*2+1,j*2+1])
     return temp
+# decrypts the encrypted image
+# by getting the values of the RGB in the 4 neighboring pixels
+# transform them back into letters
+# adds them back into a string 
+# then transform them back into integers using the dna2int() function
 def image_decrypt(im):
     temp = np.zeros((int(im.shape[0]/2),int(im.shape[1]/2),im.shape[2]),np.uint8)
     for i in range(0,im.shape[0],2):
@@ -87,73 +103,16 @@ def image_decrypt(im):
             
 
 
-im1 = im.open('graduation project/coolit.jpeg')
+im1 = im.open('coolit.jpeg')
 p1 = np.asarray(im1) 
-
-# print(p1.shape[0] , p1.shape[1] , p1.shape[2])
-print(p1[1,3])
 test = image_encrypt(p1)
-print(test[100,0])
+
 im2 = im.fromarray(test, mode="RGB")
-print(im2.getpixel((0,100)))
-im2.save("graduation project/siuu.png")
-im3 = im.open("graduation project/siuu.png")
+im2.save("encrypted_img.png")
 
+
+im3 = im.open("encrypted_img.png")
 p2 = np.asarray(im3)
-print(p2[100,0])
 test = image_decrypt(p2)
-print(test[1,3])
 im4 = im.fromarray(test, mode="RGB")
-im4.save("graduation project/siuu2.png")
-# print(im2.getpixel((4,4)))
-# print(test.shape[0] , test.shape[1] , test.shape[2])
-# print(p1[4,4])
-# # print(test[4,4])
-# test[4,4] = p1[4,4]
-# # print(test[4,4])
-        
-
-# ax = test[4,4]
-# g = eightbitbinary(ax[0])
-# g = bits2dna(g)
-# print(g)
-
-
-
-
-# # Base 2(binary)
-# bin_a = eightbitbinary(ax[1])
-# print(bin_a)
-# print(int(bin_a, 2)) 
-
-######################binary to dna test###################
-# x = 99
-# x = eightbitbinary(x)
-# print(x)
-# x = bits2dna(x)
-# print(x)
-# x = dna2int(x)
-# print(x)
-##########################################################
-# --------------------------------------
-# Creates an image memory from an object exporting the array interface (using the buffer protocol).
-
-# If obj is not contiguous, then the tobytes method is called and frombuffer() is used.
-
-# If you have an image in NumPy:
-
-# from PIL import Image
-# import numpy as np
-# im = Image.open("hopper.jpg")
-# a = np.asarray(im)
-# Then this can be used to convert it to a Pillow image:
-
-# im = Image.fromarray(a)
-# PARAMETERS:
-# obj – Object with array interface
-
-# mode –
-
-# Optional mode to use when reading obj. Will be determined from type if None.
-
-# This will not be used to convert the data after reading, but will be used to change how the data is read:
+im4.save("decryption_final.png")
