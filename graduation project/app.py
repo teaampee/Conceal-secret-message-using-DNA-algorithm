fd6520d62c63cf5fa68ff7490ac97ba85096c7d1
teaampee <ismail0w249@gmail.com>
2023-05-10 17:52:52 +0300

    added options to only do secret txt or dna

--- graduation project/app.py
+++ graduation project/app.py
@@ -149,6 +149,91 @@ def btn_encrypt():
     label4= tk.Label(top,image=im4_label).grid(column=1,row=1)
     label = tk.Label(top,text="secret message: "+msg+"\ncipher key: "+key+"\ndecrypted text: "+text).grid(column=1,row=3)
 
+def btn_encrypt_dna():
+    top = tk.Toplevel()
+    global im1,im2,im1_label,im2_label,im4_label
+    # encryption::::::::::::::::::::::::::::::::::::::::::::::::::
+    loc = select_file()
+    im1_label = Image.open(loc)
+    im1_label = im1_label.resize((400,300))
+    im1_label = ImageTk.PhotoImage(im1_label)
+    label2 = tk.Label(top,image=im1_label).grid(column=0,row=1)
+    label = tk.Label(top,text="original Image :").grid(column=0,row=0)
+    im1 = Image.open(loc)
+    p1 = np.asarray(im1) 
+    test = image_encrypt(p1)
+    im2 = Image.fromarray(test, mode="RGB")
+    dirtemp = os.path.dirname(__file__)
+    encrypted_image_name = os.path.join(dirtemp,"encrypted_img_dna"+".png")
+    im2.save(encrypted_image_name)
+    im2_label = Image.open(encrypted_image_name)
+    im2_label = im2_label.resize((400,300))
+    im2_label = ImageTk.PhotoImage(im2_label)
+    label3 = tk.Label(top,image=im2_label).grid(column=0,row=3)
+    label = tk.Label(top,text="encrypted Image :").grid(column=0,row=2)
+
+    # decreption::::::::::::::::::::::::::::::::::::::::::::
+    im3 = Image.open(encrypted_image_name)
+    p2 = np.asarray(im3)
+    p2 = p2.copy()
+    p2 = image_decrypt(p2)
+    im4 = Image.fromarray(p2, mode="RGB")
+    decryption_image_name = os.path.join(dirtemp,"decryption_final_dna"+".png")
+    im4.save(decryption_image_name)
+    im4_label = Image.open(decryption_image_name)
+    im4_label = im4_label.resize((400,300))
+    im4_label = ImageTk.PhotoImage(im4_label)
+    label = tk.Label(top,text="decrypted Image :").grid(column=1,row=0)
+    label4= tk.Label(top,image=im4_label).grid(column=1,row=1)
+
+def btn_encrypt_secret_txt():
+    top = tk.Toplevel()
+    global im1,im2,im1_label,im2_label,im4_label
+    # encryption::::::::::::::::::::::::::::::::::::::::::::::::::
+    loc = select_file()
+    im1_label = Image.open(loc)
+    im1_label = im1_label.resize((400,300))
+    im1_label = ImageTk.PhotoImage(im1_label)
+    label2 = tk.Label(top,image=im1_label).grid(column=0,row=1)
+    label = tk.Label(top,text="original Image :").grid(column=0,row=0)
+    im1 = Image.open(loc)
+    p1 = np.asarray(im1)
+    p1 = p1.copy()
+    msg = enrty_1.get()
+    key = enrty_2.get()
+    cipher = encrypt(msg,key)
+    test = merge(p1,cipher)
+    im2 = Image.fromarray(test, mode="RGB")
+    dirtemp = os.path.dirname(__file__)
+    encrypted_image_name = os.path.join(dirtemp,"encrypted_img_scrtxt"+".png")
+    im2.save(encrypted_image_name)
+    im2_label = Image.open(encrypted_image_name)
+    im2_label = im2_label.resize((400,300))
+    im2_label = ImageTk.PhotoImage(im2_label)
+    label3 = tk.Label(top,image=im2_label).grid(column=0,row=3)
+    label = tk.Label(top,text="encrypted Image :").grid(column=0,row=2)
+
+    # decreption::::::::::::::::::::::::::::::::::::::::
+    im3 = Image.open(encrypted_image_name)
+    p2 = np.asarray(im3)
+    p2 = p2.copy()
+    p2,text = unmerge(p2)
+    text = decrypt(text,key)
+    print(text)
+    im4 = Image.fromarray(p2, mode="RGB")
+    decryption_image_name = os.path.join(dirtemp,"decryption_final_srtxt"+".png")
+    im4.save(decryption_image_name)
+    im4_label = Image.open(decryption_image_name)
+    im4_label = im4_label.resize((400,300))
+    im4_label = ImageTk.PhotoImage(im4_label)
+    label = tk.Label(top,text="decrypted Image :").grid(column=1,row=0)
+    label4= tk.Label(top,image=im4_label).grid(column=1,row=1)
+    label = tk.Label(top,text="secret message: "+msg+"\ncipher key: "+key+"\ndecrypted text: "+text).grid(column=1,row=3)
+
+
+    
+
+
 
 # main program:::::::::::::::::::::::::::::::::::::::::::
 root = tk.Tk()
@@ -156,21 +241,20 @@ root.title('DNA encryption graduation project')
 
 # root.geometry("800x600")
 
-mylabel = tk.Label(root, text="Select an image to be encrypted")
-mylabel.grid(column=0,pady=5)
-# im1 = ImageTk.PhotoImage(Image.open("decryption_final.png"))
-# label1 =  tk.Label(root, image=im1)
-# label1.pack()
-
-button1 = tk.Button(root, text="Encrypt", padx= 30, command=btn_encrypt)
-button1.grid(column=0)
 mylabel = tk.Label(root, text="Enter secret message: ").grid(column=0,pady=5)
 enrty_1= tk.Entry(root,width=40)
 enrty_1.grid(column=0)
 mylabel = tk.Label(root, text="Enter playfair cipher key: ").grid(column=0,pady=5)
 enrty_2= tk.Entry(root,width=40)
 enrty_2.grid(column=0)
-
+mylabel = tk.Label(root, text="Select an image to be encrypted")
+mylabel.grid(column=0,pady=5)
+button1 = tk.Button(root, text="Encrypt", padx= 30, command=btn_encrypt)
+button1.grid(column=0)
+button2 = tk.Button(root, text="Encrypt(DNA only)", padx= 30, command=btn_encrypt_dna)
+button2.grid(column=0)
+button2 = tk.Button(root, text="Encrypt(secret text only)", padx= 30, command=btn_encrypt_secret_txt)
+button2.grid(column=0)
 
 
 root.mainloop()
